b68939314feda570aef09484cedf8086b8191e24
admin <admin>
2022-02-07 17:23:52 -0800

    Saved web app test (DKU_TSHIRTS.iYjDbeZ)

--- web_apps/iYjDbeZ/server.R
+++ web_apps/iYjDbeZ/server.R
@@ -2,7 +2,7 @@ library(shiny)
 library(dataiku)
 
 myGeoDataset <- dkuReadDataset("DKU_TSHIRTS.crm_history", samplingMethod="head", nbRows=1000)
-head(myGeoDataset)
+print(head(myGeoDataset))
 shinyServer(function(input, output) {
 
   output$mymap <- renderLeaflet({
